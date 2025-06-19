437366babe10913ab86159848e534de3dc565a43
PankajKapasiya <pankajkapasiya29@gmail.com>
2025-06-19 21:55:35 +0530

    Setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const  express= require("express");
+
+const {PORT} =require('./config/serverconfig');
+const setupAndSartServer =async ()=>{
+
+    // create the express object
+
+    const app=express();
+
+    app.listen(PORT, ()=>{
+        console.log(`Server started at ${PORT}`);
+        //console.log(process.env);
+    });
+}
+
+setupAndSartServer();
